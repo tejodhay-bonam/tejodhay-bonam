4cf3aebbb2d7f780288c0f4e3958292859b8b7b0
tejodhay-bonam <97229838+tejodhay-bonam@users.noreply.github.com>
2023-12-25 04:08:29 -0500

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -8,13 +8,25 @@ hamburger.addEventListener('click', () => {
 	mobile_menu.classList.toggle('active');
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  const header = document.getElementById('header');
+  window.onscroll = function() {
+      const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+      if (scrollPosition > 100) {
+          header.classList.add('header-scrolled');
+      } else {
+          header.classList.remove('header-scrolled');
+      }
+  };
+});
+
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 100) {
 		// header.style.backgroundImage = 'linear-gradient(45deg, #3269fd 15%, #fd8770 50%, #fff64b 80%)'; //#fd8770, #c1c1c0
     // header.style.backgroundImage = 'linear-gradient(45deg, #ef854a 15%, #3269fd 50%, #fff64b 80%)'; //option 2
     // header.style.backgroundImage = 'linear-gradient(45deg, #fff64b 15%, #3269fd 50%, #ef854a 80%)'; //option 3
-    header.style.backgroundImage = 'linear-gradient(45deg, #fff64b, #c1c1c0, #ddbbe1, #3269fd)'; //#cdb5e7, #ddbbe1
+    header.style.backgroundImage = 'linear-gradient(45deg, #fff64b, #ddbbe1, #c1c1c0, #feafa1, #3269fd)'; //#cdb5e7, #ddbbe1
 
 
 	} else {
