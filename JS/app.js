4008c3cc78b03057c6e359c6b0f9917e18d45566
tejodhay-bonam <97229838+tejodhay-bonam@users.noreply.github.com>
2023-12-25 12:37:17 -0500

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -20,19 +20,38 @@ document.addEventListener("DOMContentLoaded", function() {
   };
 });
 
+// document.addEventListener('scroll', () => {
+// 	var scroll_position = window.scrollY;
+// 	if (scroll_position > 100) {
+// 		// header.style.backgroundImage = 'linear-gradient(45deg, #3269fd 15%, #fd8770 50%, #fff64b 80%)'; //#fd8770, #c1c1c0
+//     // header.style.backgroundImage = 'linear-gradient(45deg, #ef854a 15%, #3269fd 50%, #fff64b 80%)'; //option 2
+//     // header.style.backgroundImage = 'linear-gradient(45deg, #fff64b 15%, #3269fd 50%, #ef854a 80%)'; //option 3
+//     header.style.backgroundImage = 'linear-gradient(45deg, #fff64b, #ddbbe1, #c1c1c0, #feafa1, #3269fd)'; //#cdb5e7, #ddbbe1
+
+
+// 	} else {
+// 		header.style.backgroundImage = 'none';  // Reset the background image
+//     header.style.backgroundColor = 'transparent';
+// 	}
+// });
+
 document.addEventListener('scroll', () => {
-	var scroll_position = window.scrollY;
-	if (scroll_position > 100) {
-		// header.style.backgroundImage = 'linear-gradient(45deg, #3269fd 15%, #fd8770 50%, #fff64b 80%)'; //#fd8770, #c1c1c0
-    // header.style.backgroundImage = 'linear-gradient(45deg, #ef854a 15%, #3269fd 50%, #fff64b 80%)'; //option 2
-    // header.style.backgroundImage = 'linear-gradient(45deg, #fff64b 15%, #3269fd 50%, #ef854a 80%)'; //option 3
-    header.style.backgroundImage = 'linear-gradient(45deg, #fff64b, #ddbbe1, #c1c1c0, #feafa1, #3269fd)'; //#cdb5e7, #ddbbe1
+  var scroll_position = window.scrollY;
+  const header = document.getElementById('header'); // Make sure to have the correct selector for your header
+  const isMobile = window.innerWidth <= 768; // Adjust the value according to your mobile breakpoint
 
-
-	} else {
-		header.style.backgroundImage = 'none';  // Reset the background image
-    header.style.backgroundColor = 'transparent';
-	}
+  if (scroll_position > 100) {
+      if (isMobile) {
+          // Apply mobile-specific gradient
+          header.style.backgroundImage = 'linear-gradient(45deg, #fff64b, #c1c1c0, #ddbbe1, #3269fd)';
+      } else {
+          // Apply desktop-specific gradient
+          header.style.backgroundImage = 'linear-gradient(45deg, #fff64b, #feafa1, #c1c1c0, #ddbbe1, #3269fd)';
+      }
+  } else {
+      header.style.backgroundImage = 'none'; // Reset the background image
+      header.style.backgroundColor = 'transparent';
+  }
 });
 
 menu_item.forEach((item) => {
